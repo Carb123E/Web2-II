0110f57b57834b00fa26c8fcb537adeb1be2fe06
Carb123E <2564875163@qq.com>
2024-10-11 09:54:57 +0800

    fundraiserslist api Optimization

--- node/api.js
+++ node/api.js
@@ -31,11 +31,23 @@ app.get("/api/getFundraisersListActive", (req, res) => {
 //fundraisers details by Type
 app.get("/api/getFundraisersListByType", (req, res) => {
     let param = req.query
-    let searchSql = 'SELECT * from `FUNDRAISER` WHERE FUNDRAISER_ID = ? AND CITY = ? AND CATEGORY_ID = ?'
-    DBPool.query(searchSql, [param.FUNDRAISER_ID,param.CITY,param.CATEGORY_ID], (results) => {
-        let result = results.results
-        res.send(result);
-    })
+    if (!param.FUNDRAISER_ID && !param.CITY && param.CATEGORY_ID) {
+        console.log(param);
+
+        let searchSql = 'SELECT * from `FUNDRAISER` WHERE CATEGORY_ID = ?'
+        DBPool.query(searchSql, [param.CATEGORY_ID], (results) => {
+            let result = results.results
+            res.send(result);
+        })
+    } else {
+        console.log(param);
+        let searchSql = 'SELECT * from `FUNDRAISER` WHERE FUNDRAISER_ID = ? AND CITY = ? AND CATEGORY_ID = ?'
+        DBPool.query(searchSql, [param.FUNDRAISER_ID, param.CITY, param.CATEGORY_ID], (results) => {
+            let result = results.results
+            res.send(result);
+        })
+    }
+
 });
 //fundraisers details by FUNDRAISER_ID
 app.get("/api/getFundraisersListByFUNDRAISERID", (req, res) => {
@@ -52,6 +64,26 @@ app.get("/api/getCategoriesList", (req, res) => {
     DBPool.query(searchSql, [], (results) => {
         let result = results.results
         res.send(result);
+    })
+});
+//the list of donations
+app.get("/api/getDonations", (req, res) => {
+    let searchSql = 'SELECT * from `CATEGORY`'
+    DBPool.query(searchSql, [], (results) => {
+        let result = results.results
+        res.send(result);
+    })
+});
+//add donations
+app.post("/api/addDonations", (req, res) => {
+    let param = req.body
+    var addSql =
+        'INSERT INTO `donation`(`DONATION_ID`,`DATE`,`AMOUNT`,`GIVER`,`FUNDRAISER_ID`) VALUES(?,?,?,?,?)';
+    var addSqlParams = [new Date().getTime(), param.DATE, param.AMOUNT, param.name, param.FUNDRAISER_ID];
+    DBPool.query(addSql, addSqlParams, (results) => {
+        res.send({
+            res: 'success！'
+        });
     })
 });
 
